6b4401fed2ba83c39f4951094bcff9577505082e
Garefild <Garefild2@gmail.com>
2024-10-06 06:00:20 +0300

    xbuild: build esm and cjs

--- xbuild.config.ts
+++ xbuild.config.ts
@@ -9,17 +9,24 @@ import type { xBuildConfig } from '@remotex-labs/xbuild';
  */
 
 import { version } from 'process';
+import pkg from './package.json' with { type: 'json' };
+
+/**
+ * Config build
+ */
 
 const config: xBuildConfig = {
     declaration: true,
     esbuild: {
         bundle: true,
         minify: true,
-        format: 'cjs',
+        format: 'esm',
+        outdir: 'dist/esm',
         target: [ `node${ version.slice(1) }` ],
         platform: 'node',
         packages: 'external',
-        sourcemap: true,
+        sourcemap: 'external',
+        sourceRoot: `https://github.com/remotex-lab/xmap/tree/${ pkg.version }/`,
         entryPoints: [ 'src/index.ts' ]
     }
 };
