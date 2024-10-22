52bb00d84aaf48c80f987bafae1e98780add2d28
Garefild <Garefild2@gmail.com>
2024-10-23 01:14:55 +0300

    xBuild: use a multi build system at ones

--- xbuild.config.ts
+++ xbuild.config.ts
@@ -15,20 +15,40 @@ import pkg from './package.json' with { type: 'json' };
  * Config build
  */
 
-const config: xBuildConfig = {
-    declaration: true,
-    esbuild: {
-        bundle: true,
-        minify: true,
-        format: 'esm',
-        outdir: 'dist/esm',
-        target: [ `node${ version.slice(1) }` ],
-        platform: 'node',
-        packages: 'external',
-        sourcemap: 'external',
-        sourceRoot: `https://github.com/remotex-lab/xmap/tree/${ pkg.version }/`,
-        entryPoints: [ 'src/index.ts' ]
+const config: Array<xBuildConfig> = [
+    {
+        declaration: true,
+        esbuild: {
+            bundle: false,
+            minify: true,
+            format: 'esm',
+            outdir: 'dist',
+            target: [ `node${ version.slice(1) }` ],
+            platform: 'node',
+            packages: 'external',
+            sourcemap: true,
+            sourceRoot: `https://github.com/remotex-lab/xmap/tree/${ pkg.version }/`,
+            entryPoints: [ 'src/index.ts' ]
+        }
+    },
+    {
+        declaration: false,
+        noTypeChecker: false,
+        esbuild: {
+            bundle: true,
+            format: 'esm',
+            outdir: 'dist/esm'
+        }
+    },
+    {
+        declaration: false,
+        noTypeChecker: false,
+        esbuild: {
+            bundle: true,
+            format: 'cjs',
+            outdir: 'dist/cjs'
+        }
     }
-};
+];
 
 export default config;
